2f8dc839f4728dd859cbbcb9d3a82b0981585b5e
objectundefined <gabriel.lipson@gmail.com>
2014-03-22 17:18:41 -0700

    repl improvements. supports multiline and params

--- repl.js
+++ repl.js
@@ -15,19 +15,42 @@ neo.createConnection("tcp://localhost:47474",{poolSize:1},function(err,graph){
       if (err) throw err;
     });
   });
+  var tmp = [];
+  var params = {};
   function eval(str, context, file, cb) {
-    var t1 = Date.now();
+    var t1 = Date.now(),query,p;
     str = str.slice(1).slice(0, -2);
-    graph.query(str,function(err,d,info){
-      if(err) return cb(err);
-      var t2 = Date.now()
-      var roudTripTime = (t2-t1) + "ms";
+    
+    if (str.indexOf(";")==str.length-1) {
+      str = str.slice(0,-1);
+      tmp.push(str);
+      query = tmp.splice(0).join("\n");
+      p = params;
+      params={};
+      graph.query(query,p,function(err,d,info){
+        if(err) return cb(err);
+        var t2 = Date.now()
+        var roudTripTime = (t2-t1) + "ms";
       
-      console.log("results:\n%s\n", JSON.stringify(d,null,4));
-      console.log("info:\n%s\n", JSON.stringify(info,null,4));
+        console.log("results:\n%s\n", JSON.stringify(d,null,4));
+        console.log("info:\n%s\n", JSON.stringify(info,null,4));
       
+        cb();
+      });
+    } else if (str.indexOf("$PARAMS=")==0) {
+      
+      try {
+        params = JSON.parse(str.slice(8));
+        cb();
+      } catch(e){
+        return cb(e);
+      }
+      
+    } else {
+      tmp.push(str);
       cb();
-    });
+    }
+    
   }
   
 });
